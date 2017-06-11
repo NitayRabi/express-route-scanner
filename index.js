16eb3d81c816578c36883f6cb74374c04f5b6d09
Nitay Rabinovich <nitayrabi@gmail.com>
2017-06-11 12:21:49 +0300

    inital commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import * as express from 'express';
+
+exports.getAppRoutes = function(expressApp) {
+            // TBD - recurse over route tree to replace triple loop.
+            const result = [];
+
+            expressApp._router.stack.forEach(layer => {
+                if (layer.route) {
+                    result.push(layer);
+                } else if (layer.name === 'router') {
+                    const regexp = layer.regexp.source;
+                    const prefix_routes = regexp.match(/(\w+)+/gi);
+                    let prefix = '';
+                    if (prefix_routes !== null) {
+                        prefix = `/${prefix_routes[0]}/${prefix_routes[1]}`;
+                    }
+                    layer.handle.stack.forEach(handler => {
+                        if (handler.route.stack) {
+                            handler.route.stack.forEach(endRoute => {
+                                const method = endRoute.method ? endRoute.method.toUpperCase() : null;
+                                const route = handler.route.path;
+                                result.push({method: method, path: prefix + route});
+                            });
+                        }
+                    });
+                }
+            });
+            return result;
+        };
